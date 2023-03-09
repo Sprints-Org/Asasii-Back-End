b6d01a9608d66f1fae902a52f54dd4a94ae07e45
Ahmed Rabea Smaha <35260162+ahmedrabeasmaha@users.noreply.github.com>
2023-03-09 02:04:40 +0200

    compile cors 4

--- dist/Model/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductModel = exports.CategoryModel = exports.UserModel = void 0;
-var UserModel_1 = require("./UserModel");
-Object.defineProperty(exports, "UserModel", { enumerable: true, get: function () { return UserModel_1.UserModel; } });
-var CategoryModel_1 = require("./CategoryModel");
-Object.defineProperty(exports, "CategoryModel", { enumerable: true, get: function () { return CategoryModel_1.CategoryModel; } });
-var ProductModel_1 = require("./ProductModel");
-Object.defineProperty(exports, "ProductModel", { enumerable: true, get: function () { return ProductModel_1.ProductModel; } });
